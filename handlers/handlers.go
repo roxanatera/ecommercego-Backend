4455d28b415e56cce94b004e5089192929fb12bb
Julia Roxana Natera <103115280+roxanatera@users.noreply.github.com>
2025-04-03 12:49:08 +0200

    update

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/aws/aws-lambda-go/events"
 	"ecommerce-Backend/routers"
-	
+
+	"github.com/aws/aws-lambda-go/events"
 )
 
 func Manejadores(path string, method string, body string, headers map[string]string, request events.APIGatewayV2HTTPRequest) (int, string) {
@@ -20,7 +20,7 @@ func Manejadores(path string, method string, body string, headers map[string]str
 	if !isOk {
 		return statusCode, user
 	}
-	switch path[0:4] {
+	switch path[1:5] {
 	case "user":
 		return ProcesoUsers(body, path, method, user, id, request)
 	case "product":
@@ -33,8 +33,8 @@ func Manejadores(path string, method string, body string, headers map[string]str
 		return ProcesoOrder(body, path, method, user, idn, request)
 	case "cate":
 		return ProcesoCategory(body, path, method, user, idn, request)
-	
-}	
+
+	}
 
 	return 400, "Method Invalid"
 
@@ -42,53 +42,51 @@ func Manejadores(path string, method string, body string, headers map[string]str
 
 func validoAuthorization(path string, method string, body string, headers map[string]string) (bool, int, string) {
 	if (path == "product" && method == "GET") ||
-	   (path == "category" && method == "GET") {
+		(path == "category" && method == "GET") {
 		return true, 200, "OK"
-	   }
-	
-	   token := headers["Authorization"]
-	   if len(token) == 0 {
-		return false, 401, "Token Requerido"
-	   }
+	}
 
-	   todoOk, err, msg := auth.ValidoToken(token)
-	   if !todoOk {
+	token := headers["Authorization"]
+	if len(token) == 0 {
+		return false, 401, "Token Requerido"
+	}
+
+	todoOk, err, msg := auth.ValidoToken(token)
+	if !todoOk {
 		if err != nil {
 			fmt.Println("Error: ", err.Error())
-		return false, 401, err.Error()
-	   }else{
+			return false, 401, err.Error()
+		} else {
 			fmt.Println("Error en el token: ", msg)
-	   }
+		}
 	}
 	fmt.Println("Token valido: ", msg)
 	return true, 200, msg
 }
 
 func ProcesoUsers(body string, path string, method string, user string, id string, request events.APIGatewayV2HTTPRequest) (int, string) {
-	
-		return 400, "Method Invalid"
-	}
+
+	return 400, "Method Invalid"
+}
 func ProcesoProducts(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
-	
-		return 400, "Method Invalid"
-	}
+
+	return 400, "Method Invalid"
+}
 func ProcesoStock(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
 	return 400, "Method Invalid"
-	}
+}
 func ProcesoAddress(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
 	return 400, "Method Invalid"
-	}	
+}
 
 func ProcesoOrder(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
 
 	return 400, "Method Invalid"
-	}
+}
 func ProcesoCategory(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
 	switch method {
 	case "POST":
 		return routers.InsertCategory(body, user)
-}
-	return 400, "Method Invalid"
 	}
-
-
+	return 400, "Method Invalid"
+}
